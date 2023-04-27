d02a393a5e515a69ff3f9aa9fe4743b2f448b642
Uriel Rivera <94717788+urielzrivera@users.noreply.github.com>
2023-04-27 14:45:32 -0700

    Update csv_combined.py

--- python/csv_combined.py
+++ python/csv_combined.py
@@ -8,12 +8,6 @@ import pandas as pd
 # master dataframe
 master_df = pd.DataFrame()
 
-# setting variable for path to csv files
-''' path = 'C:\Users\<user_directory>\Downloads'
-for file in os.listdir(path):
-          if file.endswith('.csv'):
-                    master_df.append(pd.read_csv(files)) '''
-
 # reads and merges csv files from current working directory
 for file in os.listdir(getcwd()):
           if file.endswith('.csv'):
